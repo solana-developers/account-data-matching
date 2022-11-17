7fb92446e90eb74ff12d2180251c424bf5839d11
john <johnzhengyuliu@gmail.com>
2022-11-17 07:39:38 -0600

    draft

---
+++ programs/account-data-matching/src/lib.rs
@@ -0,0 +1,133 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod account_data_matching {
+    use super::*;
+
+    pub fn initialize_vault(ctx: Context<InitializeVault>) -> Result<()> {
+        ctx.accounts.vault.token_account = ctx.accounts.token_account.key();
+        ctx.accounts.vault.authority = ctx.accounts.authority.key();
+        ctx.accounts.vault.withdraw_destination = ctx.accounts.withdraw_destination.key();
+        Ok(())
+    }
+
+    pub fn insecure_withdraw(ctx: Context<InsecureWithdraw>) -> Result<()> {
+        let amount = ctx.accounts.token_account.amount;
+
+        let seeds = &[b"vault".as_ref(), &[*ctx.bumps.get("vault").unwrap()]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.withdraw_destination.to_account_info(),
+            },
+            &signer,
+        );
+
+        token::transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn secure_withdraw(ctx: Context<SecureWithdraw>) -> Result<()> {
+        let amount = ctx.accounts.token_account.amount;
+
+        let seeds = &[b"vault".as_ref(), &[*ctx.bumps.get("vault").unwrap()]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.withdraw_destination.to_account_info(),
+            },
+            &signer,
+        );
+
+        token::transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeVault<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 32 + 32,
+        seeds = [b"vault"],
+        bump,
+    )]
+    pub vault: Account<'info, Vault>,
+    #[account(
+        init,
+        payer = authority,
+        token::mint = mint,
+        token::authority = vault,
+        seeds = [b"token"],
+        bump,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub withdraw_destination: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct InsecureWithdraw<'info> {
+    #[account(
+        seeds = [b"vault"],
+        bump,
+    )]
+    pub vault: Account<'info, Vault>,
+    #[account(
+        mut,
+        seeds = [b"token"],
+        bump,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub withdraw_destination: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SecureWithdraw<'info> {
+    #[account(
+        seeds = [b"vault"],
+        bump,
+        has_one = token_account,
+        has_one = authority,
+        has_one = withdraw_destination,
+
+    )]
+    pub vault: Account<'info, Vault>,
+    #[account(
+        mut,
+        seeds = [b"token"],
+        bump,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub withdraw_destination: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Vault {
+    token_account: Pubkey,
+    authority: Pubkey,
+    withdraw_destination: Pubkey,
+}
---
+++ tests/account-data-matching.ts
@@ -0,0 +1,149 @@
+import * as anchor from "@project-serum/anchor"
+import * as spl from "@solana/spl-token"
+import { Program } from "@project-serum/anchor"
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey"
+import { AccountDataMatching } from "../target/types/account_data_matching"
+import { expect } from "chai"
+
+describe("account-data-matching", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env())
+
+  const connection = anchor.getProvider().connection
+  const wallet = anchor.workspace.AccountDataMatching.provider.wallet
+  const walletFake = anchor.web3.Keypair.generate()
+
+  const program = anchor.workspace
+    .AccountDataMatching as Program<AccountDataMatching>
+
+  const [vaultPDA] = findProgramAddressSync(
+    [Buffer.from("vault")],
+    program.programId
+  )
+
+  const [tokenPDA] = findProgramAddressSync(
+    [Buffer.from("token")],
+    program.programId
+  )
+
+  let mint: anchor.web3.PublicKey
+  let withdrawDestination: anchor.web3.PublicKey
+  let withdrawDestinationFake: anchor.web3.PublicKey
+
+  before(async () => {
+    mint = await spl.createMint(
+      connection,
+      wallet.payer,
+      wallet.publicKey,
+      null,
+      0
+    )
+
+    withdrawDestination = await spl.createAccount(
+      connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey
+    )
+
+    withdrawDestinationFake = await spl.createAccount(
+      connection,
+      wallet.payer,
+      mint,
+      walletFake.publicKey
+    )
+
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(
+        walletFake.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    )
+  })
+
+  it("Initialize Vault", async () => {
+    await program.methods
+      .initializeVault()
+      .accounts({
+        vault: vaultPDA,
+        tokenAccount: tokenPDA,
+        withdrawDestination: withdrawDestination,
+        mint: mint,
+        authority: wallet.publicKey,
+      })
+      .rpc()
+
+    await spl.mintTo(
+      connection,
+      wallet.payer,
+      mint,
+      tokenPDA,
+      wallet.payer,
+      100
+    )
+
+    const balance = await connection.getTokenAccountBalance(tokenPDA)
+    expect(balance.value.uiAmount).to.eq(100)
+  })
+
+  it("Insecure withdraw", async () => {
+    const tx = await program.methods
+      .insecureWithdraw()
+      .accounts({
+        vault: vaultPDA,
+        tokenAccount: tokenPDA,
+        withdrawDestination: withdrawDestinationFake,
+        authority: walletFake.publicKey,
+      })
+      .transaction()
+
+    await anchor.web3.sendAndConfirmTransaction(connection, tx, [walletFake])
+
+    const balance = await connection.getTokenAccountBalance(tokenPDA)
+    expect(balance.value.uiAmount).to.eq(0)
+  })
+
+  it("Secure withdraw, expect error", async () => {
+    try {
+      const tx = await program.methods
+        .secureWithdraw()
+        .accounts({
+          vault: vaultPDA,
+          tokenAccount: tokenPDA,
+          withdrawDestination: withdrawDestinationFake,
+          authority: walletFake.publicKey,
+        })
+        .transaction()
+
+      await anchor.web3.sendAndConfirmTransaction(connection, tx, [walletFake])
+    } catch (err) {
+      expect(err)
+      console.log(err)
+    }
+  })
+
+  it("Secure withdraw", async () => {
+    await spl.mintTo(
+      connection,
+      wallet.payer,
+      mint,
+      tokenPDA,
+      wallet.payer,
+      100
+    )
+
+    await program.methods
+      .secureWithdraw()
+      .accounts({
+        vault: vaultPDA,
+        tokenAccount: tokenPDA,
+        withdrawDestination: withdrawDestination,
+        authority: wallet.publicKey,
+      })
+      .rpc()
+
+    const balance = await connection.getTokenAccountBalance(tokenPDA)
+    expect(balance.value.uiAmount).to.eq(0)
+  })
+})
